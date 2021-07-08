7b9ff066ad3797f72279e181bef42b634ba3042f
Azu0925 <yamaneko564783@gmail.com>
2021-07-08 19:29:36 +0900

    インデント修正

--- src/components/logins/Login.vue
+++ src/components/logins/Login.vue
@@ -7,14 +7,14 @@
       </div>
     </div>
     <div class="panels-container">
-        <div class="panel left-panel">
-          <SignUpSelector />
-            <img src="@/assets/img/security_SVG.svg" class="image" alt="" />
-        </div>
-        <div class="panel right-panel">
-          <SignInSelector />
-          <img src="@/assets/img/app_development_SVG.svg" class="image" alt="" />
-        </div>
+      <div class="panel left-panel">
+        <SignUpSelector />
+        <img src="@/assets/img/security_SVG.svg" class="image" alt="" />
+      </div>
+      <div class="panel right-panel">
+        <SignInSelector />
+        <img src="@/assets/img/app_development_SVG.svg" class="image" alt="" />
+      </div>
     </div>
   </div>
 </template>
