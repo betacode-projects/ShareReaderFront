1e414e0fa547cae282da8e76e0a7350d042bb69a
hirosuke-pi <31305419+betacode-projects@users.noreply.github.com>
2021-07-10 11:42:53 +0900

    Vueテンプレート化 (#1)
    * first commit
    
    * yaml作成途中
    
    * よくわからんけどとりあえず
    
    * cloudbuild.yaml修正
    
    * ビルドしたもののみになるように
    
    * ビルド後パスが通らないバグ解消
    
    * a
    
    * vue.config.js削除
    
    * 404 feature/#7 (#10)
    
    * 404
    
    * 色変更
    
    * 多分最終
    
    * 404修正
    
    * vue_oazuke
    
    * 途中
    
    * あらかた完成
    
    * topページ view完成
    
    * 消し忘れ削除
    
    Co-authored-by: betacode-projects <betacode-projects.net@gmail.com>
    Co-authored-by: Azu0925 <yamaneko564783@gmail.com>
    Co-authored-by: shiroa22 <nbshirocircle@icloud.com>
    Co-authored-by: Àżu <53806909+Azu0925@users.noreply.github.com>

---
+++ src/components/footers/footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+    <div class="container w-container">
+      <div class="sitefooter__row sitefooter__row--copyright footer-text">
+        <div class="sitefooter__column sitefooter__column--copyright">
+          <div class="copyirght">
+            <div id="footer-c">© 2021 takahiro</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'footer'
+}
+</script>
+<style>
+@import "../../assets/css/fr.css";
+@import "../../assets/css/style.css";
+@import "../../assets/css/responsive.css";
+@import "../../assets/css/main.css";
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import homePage from '@/components/homePages/homePage'
+import NotFoundComponent from '@/components/NotFound'
 
 Vue.use(Router)
 
@@ -8,8 +10,18 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'homePage',
+      component: homePage
+    },
+    {
+      path: '/helloworld',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '*',
+      name: 'NotFound',
+      component: NotFoundComponent
     }
   ]
 })
