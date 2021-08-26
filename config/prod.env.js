cc6f49eaa43400298e5228d7dac9f92eb23e7b76
Azu0925 <yamaneko564783@gmail.com>
2021-08-26 12:42:23 +0900

    なんとか強引に

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  SOCKET_URL: '"wss://share-reader-socket-4bifvghjoq-an.a.run.app"',
+  SOCKET_URL: '"ws://34.97.24.140"',
   API_URL: '"https://share-reader-api-4bifvghjoq-an.a.run.app/v1/api"'
 }
