707cf069c9f2f839a41cc2cb84152179ec68a548
Azu0925 <yamaneko564783@gmail.com>
2021-08-26 23:11:06 +0900

    これでどや

--- src/components/homePages/topContainer.vue
+++ src/components/homePages/topContainer.vue
@@ -21,7 +21,7 @@
 </template>
 
 <script>
-import QrModal from '@/components/modulesqrModal'
+import QrModal from '@/components/modules/qrModal'
 import DownloadButton from './DownloadButton'
 import {URL, SENDER} from '../../define/config'
 import axios from 'axios'
