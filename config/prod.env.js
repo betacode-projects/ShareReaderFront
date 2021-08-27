acc1668a3961d1f4f81c57593aea0c397dd4c2b0
Azu0925 <yamaneko564783@gmail.com>
2021-08-27 13:35:05 +0900

    phoenix app engineに移行

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  SOCKET_URL: '"wss://share-reader-socket-4bifvghjoq-an.a.run.app"',
+  SOCKET_URL: '"wss://share-reader-socket-dot-share-reader.dt.r.appspot.com"',
   API_URL: '"https://share-reader-api-4bifvghjoq-an.a.run.app/v1/api"'
 }
